66e5a42a260ee96540e0da42e7df82b2027002d8
Priyansh <priyanshg615@gmail.com>
2022-03-03 01:22:19 +0530

    Yargs

--- FirstScript/notes-app/app.js
+++ FirstScript/notes-app/app.js
@@ -1,8 +1,13 @@
-const validator = require("validator");
-const add = require("./utils");
+// const validator = require("validator");
+// const add = require("./utils");
 const chalk = require("chalk");
+const notes = require("./notes");
 
-console.log(chalk.green("Success!!!"));
-console.log(chalk.yellow.inverse("Fuck You"));
-const sum = add(4, 6);
-console.log(sum);
+const command = process.argv[2];
+console.log(command);
+if (command === "add") console.log("Adding");
+else console.log("Removing");
+// console.log(chalk.green("Success!!!"));
+// console.log(chalk.yellow.inverse("Fuck You"));
+// const sum = add(4, 6);
+// console.log(sum);
