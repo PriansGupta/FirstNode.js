d91f260381c219541d2b761da071c30a00103f9c
Priyansh <priyanshg615@gmail.com>
2022-03-11 01:05:11 +0530

    Weather App

--- WeatherApp/Geocode.js
+++ WeatherApp/Geocode.js
@@ -3,7 +3,7 @@ const weather=require('./Weather')
 
 const Geocode = (address, callback) => {
     const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?limit=1&access_token=pk.eyJ1IjoicHJpYW5zIiwiYSI6ImNsMGp4dWdwajBoMWkzZHVvM2IwenZkc3oifQ.jLETuenG2e14hTyin_oJCA`;
-
+    
     request({ url: url, json: true }, (error, response) => {
       if (error) callback("Unable to connect", undefined);
       else if (response.body.features.length === 0)
--- WeatherApp/Weather.js
+++ WeatherApp/Weather.js
@@ -1,12 +1,12 @@
 const request = require("request");
 
-
 const weather = (address, callback) => {
-  const url =
-    "http://api.weatherstack.com/current?access_key=69deb58529f2280c0a95575913b5df3d&query=26.215,78.19306";
+  const url = `http://api.weatherstack.com/current?access_key=69deb58529f2280c0a95575913b5df3d&query=${address.lon},${address.lat}`;
+
 
   request({ url: url, json: true }, (error, response) => {
     if (error) callback("Unable to connect", undefined);
+    else if (response.body.error) callback("Unable to connect", undefined);
     else {
       const Temperature = response.body.current.temperature;
       const description = response.body.current.weather_descriptions[0];
@@ -15,7 +15,6 @@ const weather = (address, callback) => {
         Temperature: Temperature,
         description: description,
       });
-      // console.log(Temperature,description)
     }
   });
 };
