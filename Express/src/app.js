a9ebe59ea7c175c4410173c65899015fee6b0515
Priyansh <priyanshg615@gmail.com>
2022-03-12 01:29:43 +0530

    Server

--- Express/src/app.js
+++ Express/src/app.js
@@ -1,24 +1,10 @@
 const express = require("express");
-const path=require('path')
+const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname,'../public')))
+app.use(express.static(path.join(__dirname, "../public")));
 
-app.get("", (req, res) => {
-  res.send('<h1>HOME</h1>');
+app.listen(3000, () => {
+  console.log("Server is Up");
 });
-
-app.get("/About", (req, res) => {
-  res.send('<h1>About</h1>');
-});
-app.get("/Help", (req, res) => {
-  res.send("Help Page");
-});
-app.get("/Weather", (req, res) => {
-  res.send("Weather Page");
-});
-
-app.listen(3000,()=>{
-    console.log("Server is Up")
-})
