b7e9167b6134a67260706098145b637f3e9ae929
Priyansh <priyanshg615@gmail.com>
2022-03-12 01:19:58 +0530

    Server

--- Express/src/app.js
+++ Express/src/app.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const path=require('path')
 
 const app = express();
+
+console.log(__dirname)
+console.log(path.join(__dirname,'../public'))
+
 
 app.get("", (req, res) => {
   res.send('<h1>HOME</h1>');
