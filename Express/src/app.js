522773a6b1a1505978278d044c099eeb4ab139d7
Priyansh <priyanshg615@gmail.com>
2022-03-13 17:30:58 +0530

    Handle Bars

--- Express/src/app.js
+++ Express/src/app.js
@@ -3,7 +3,14 @@ const path = require("path");
 
 const app = express();
 
+app.set('view engine','hbs')
 app.use(express.static(path.join(__dirname, "../public")));
+app.set('views', path.join(__dirname, '../public/views'));
+
+
+app.get('',(req,res)=>{
+  res.render('index')
+})
 
 app.listen(3000, () => {
   console.log("Server is Up");
