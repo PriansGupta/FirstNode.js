5e7fee9cfc2ea501b45bb096541cf645746b6aa3
Priyansh <priyanshg615@gmail.com>
2022-03-14 23:43:27 +0530

    Handle Bars

--- Express/src/app.js
+++ Express/src/app.js
@@ -14,8 +14,7 @@ app.use(express.static(path.join(__dirname, "../public")));
 
 app.get("", (req, res) => {
   res.render("index", {
-    Title: "Home",
-    Name: "Priyansh Gupta",
+    Title: "Weather",
   });
 });
 
