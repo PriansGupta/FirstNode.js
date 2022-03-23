a089221af55165900aacc1eba5794c71c5ce29ae
Priyansh <priyanshg615@gmail.com>
2022-03-23 17:11:38 +0530

    MongoDB TaskManager

--- MongoDb/TaskManager/mongodb.js
+++ MongoDb/TaskManager/mongodb.js
@@ -1,5 +1,5 @@
 const mongobd = require("mongodb");
-const MongoClient = mongobd.mongoClient;
+const MongoClient = mongobd.MongoClient;
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const database = "taskManager";
