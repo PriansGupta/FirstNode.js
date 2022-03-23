3166c43ba5b018dca7815f4b4b78745dda598e23
Priyansh <priyanshg615@gmail.com>
2022-03-23 17:07:18 +0530

    MongoDB TaskManager

--- MongoDb/TaskManager/mongodb.js
+++ MongoDb/TaskManager/mongodb.js
@@ -4,6 +4,9 @@ const MongoClient=mongobd.mongoClient;
 const connectionURL="mongodb://127.0.0.1:27017"
 const database="taskManager"
 
-MongoClient.connect(connectionURL,{useNewUrlParser:true},()=>{
-    
+MongoClient.connect(connectionURL,{useNewUrlParser:true},(error,client)=>{
+    if(error){
+        return console.log("Unable to Connect to DataBase")
+    }
+
 })
