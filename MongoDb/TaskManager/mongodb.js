920cbca38c0083713fc8d55ec80351911813d9c9
Priyansh <priyanshg615@gmail.com>
2022-03-24 23:03:42 +0530

    MongoDB TaskManager

--- MongoDb/TaskManager/mongodb.js
+++ MongoDb/TaskManager/mongodb.js
@@ -4,13 +4,13 @@
 // const MongoClient = mongobd.MongoClient;
 // const ObjectID = mongobd.ObjectID;
 
-const {MongoClient,ObjectID}=require('mongodb')
+const { MongoClient, ObjectID } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const database = "taskManager";
 
 const id = new ObjectID();
-console.log("ID:"+id);
+console.log("ID:" + id);
 console.log(id.getTimestamp());
 
 MongoClient.connect(
@@ -25,48 +25,10 @@ MongoClient.connect(
 
     const db = client.db(database);
 
-    // db.collection("users").insertMany(
-    //   [
-    //     {
-    //       name: "Priyansh",
-    //       age: 20,
-    //     },
-    //     {
-    //       name: "Ruhi",
-    //       age: 27,
-    //     },
-    //   ],
-    //   (error, result) => {
-    //     if (error) return console.log("Unable to insert the document");
+    db.collection("users").findOne({ age: 20 }, (error, user) => {
+      if (error) return console.log("UNABLE TO FETCH");
 
-    //     console.log(result.ops);
-    //   }
-    // );
-
-    // db.collection("Tasks").insertMany(
-    //   [
-    //     {
-    //       ToDo: "Practical",
-    //       status: false,
-    //     },
-    //     {
-    //       ToDo: "End Sem",
-    //       status: true,
-    //     },
-    //   ],
-    //   (error, result) => {
-    //     if (error) return console.log("Unable to insert the document");
-
-    //     console.log(result.ops);
-    //   }
-    // );
-
-    db.collection('users').findOne({age:20},(error,user)=>{
-        if(error)
-        return console.log("UNABLE TO FETCH")
-
-        console.log(user)
-    })
+      console.log(user);
+    });
   }
 );
-
