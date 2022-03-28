aa738efc0f7ad6eb2ec00956a5120a8f6908007a
Priyansh <priyanshg615@gmail.com>
2022-03-28 23:37:06 +0530

    Mongoose

--- MongoDb/TaskManager/src/db/mongoose.js
+++ MongoDb/TaskManager/src/db/mongoose.js
@@ -1,15 +1,15 @@
-const mongoose=require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api",{
-    useNewUrlParser:true,
-    useCreateIndex:true
-})
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
 
-const User=mongoose.model('User',{
-    name:{
-   type:String
-    },
-    age:{
-  type:Number
-    }
-})
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+  },
+  age: {
+    type: Number,
+  },
+});
