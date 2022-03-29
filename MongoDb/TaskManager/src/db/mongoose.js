3226be6dd63c5818d24556d87581a2e9d0c04f84
Priyansh <priyanshg615@gmail.com>
2022-03-29 23:35:33 +0530

    Mongoose

--- MongoDb/TaskManager/src/db/mongoose.js
+++ MongoDb/TaskManager/src/db/mongoose.js
@@ -41,10 +41,10 @@ const me = new User({
   age: 47,
 });
 
-// me.save()
-//   .then(() => {
-//     console.log(me);
-//   })
-//   .catch((error) => {
-//     console.loh(error);
-//   });
+me.save()
+  .then(() => {
+    console.log(me);
+  })
+  .catch((error) => {
+    console.loh(error);
+  });
