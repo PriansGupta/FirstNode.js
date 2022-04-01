b04a97a72cbf9395956b01ef649fd08a53936d79
Priyansh <priyanshg615@gmail.com>
2022-04-01 22:47:55 +0530

    Mongoose

--- MongoDb/TaskManager/src/index.js
+++ MongoDb/TaskManager/src/index.js
@@ -3,6 +3,11 @@ const express=require("express")
 const app=express();
 const port=process.env.PORT || 3000;
 
+app.post('/users',(req,res)=>{
+
+    res.send("Testing!")
+})
+
 app.listen(port,()=>{
 console.log("Server is Up!")
 })
