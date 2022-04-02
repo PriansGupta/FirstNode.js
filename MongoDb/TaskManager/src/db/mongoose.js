fd214898530d91895f4ea631ebe5061132de0d9e
Priyansh <priyanshg615@gmail.com>
2022-04-02 21:11:43 +0530

    Mongoose

--- MongoDb/TaskManager/src/db/mongoose.js
+++ MongoDb/TaskManager/src/db/mongoose.js
@@ -7,7 +7,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 });
 
 
-
 const Task = mongoose.model("Tasks", {
   Description: {
     type: String,
--- MongoDb/TaskManager/src/index.js
+++ MongoDb/TaskManager/src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+require('../src/db/mongoose')
+const User=require('../src/models/user')
 
 const app = express();
 const port = process.env.PORT || 3000;
