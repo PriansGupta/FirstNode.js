c53bff80753edd5800ded79c59e957c52b728bcb
Priyansh <priyanshg615@gmail.com>
2022-04-02 21:22:52 +0530

    Mongoose

--- MongoDb/TaskManager/src/db/mongoose.js
+++ MongoDb/TaskManager/src/db/mongoose.js
@@ -1,16 +1,14 @@
 const mongoose = require("mongoose");
 
-
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true,
 });
 
-
 const Task = mongoose.model("Tasks", {
   Description: {
     type: String,
-    required:true
+    required: true,
   },
   Completed: {
     type: Boolean,
