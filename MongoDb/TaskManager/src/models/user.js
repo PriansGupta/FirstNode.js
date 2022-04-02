5e73debeeb9f0129ce20e0cefed9144902399e05
Priyansh <priyanshg615@gmail.com>
2022-04-02 21:23:20 +0530

    Mongoose

--- MongoDb/TaskManager/src/models/user.js
+++ MongoDb/TaskManager/src/models/user.js
@@ -1,27 +1,26 @@
 const mongoose = require("mongoose");
-const validator=require("validator")
+const validator = require("validator");
 
 const User = mongoose.model("User", {
-    name: {
-      type: String,
-      required:true
-    },
-    email:{
-        type:String,
-        required:true,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error("Not a Valid Email")
-            }
-        }
-    },
-    age: {
-      type: Number,
-      validate(value){
-      if(value<0)
-      throw new Error("Age must be Positive")
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Not a Valid Email");
       }
     },
-  });
+  },
+  age: {
+    type: Number,
+    validate(value) {
+      if (value < 0) throw new Error("Age must be Positive");
+    },
+  },
+});
 
-  module.exports=User
+module.exports = User;
