99139a66daeb38689f4817e7523f69bff3a3392f
Priyansh <priyanshg615@gmail.com>
2022-04-02 21:13:39 +0530

    Mongoose

--- MongoDb/TaskManager/src/index.js
+++ MongoDb/TaskManager/src/index.js
@@ -1,12 +1,22 @@
+const e = require("express");
 const express = require("express");
-require('../src/db/mongoose')
-const User=require('../src/models/user')
+require("../src/db/mongoose");
+const User = require("../src/models/user");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.post("/users", (req, res) => {
-  res.send("Testing!");
+  const user = new User(req.body);
+
+  user
+    .save()
+    .then(() => {
+      res.send(user);
+    })
+    .catch((e) => {
+      res.send(e);
+    });
 });
 
 app.listen(port, () => {
