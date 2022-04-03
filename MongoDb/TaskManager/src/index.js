9236bf08c382d1855260662fc05be6687c596da8
Priyansh <priyanshg615@gmail.com>
2022-04-03 18:48:03 +0530

    Mongoose

--- MongoDb/TaskManager/src/index.js
+++ MongoDb/TaskManager/src/index.js
@@ -3,7 +3,6 @@ require("../src/db/mongoose");
 const User = require("../src/models/user");
 const Task = require("../src/models/tasks");
 
-
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -22,18 +21,18 @@ app.post("/users", (req, res) => {
     });
 });
 
-app.post("/tasks",(req,res)=>{
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
 
-    const task=new Task(req.body);
-
-    task.save().then(()=>{
-        res.send(task)
-    }).catch((e)=>{
-        res.status(400).send(e)
+  task
+    .save()
+    .then(() => {
+      res.send(task);
     })
-
-})
-
+    .catch((e) => {
+      res.status(400).send(e);
+    });
+});
 
 app.listen(port, () => {
   console.log("Server is Up!");
