75c9f510f08ce014994549be3e5e4c0f557100d9
Priyansh <priyanshg615@gmail.com>
2022-04-04 19:37:10 +0530

    Mongoose

--- MongoDb/TaskManager/playground/promiseChaining.js
+++ MongoDb/TaskManager/playground/promiseChaining.js
@@ -1,3 +1,7 @@
 require("../src/db/mongoose")
 const User=require("../src/models/user")
 
+User.findById("62449aab9bf8be08bc318032",{age:1}).then((user)=>{
+    console.log(user)
+    return User.countDocuments({age:1})
+})
