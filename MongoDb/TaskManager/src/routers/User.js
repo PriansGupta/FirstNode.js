aeacb8ad807bad94b8a4d32690568bb73cc8ea22
Priyansh <priyanshg615@gmail.com>
2022-04-09 23:16:49 +0530

    Mongoose

--- MongoDb/TaskManager/src/routers/User.js
+++ MongoDb/TaskManager/src/routers/User.js
@@ -12,4 +12,19 @@ app.post("/users", async (req, res) => {
       res.status(404).send(e);
     }
   });
+
+
+  app.get("/users/:id", async (req, res) => {
+    const _id = req.params.id;
+  
+    try {
+      const user = await User.findById(_id);
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.status(200).send("User Found: " + user);
+    } catch (e) {
+      res.status(404).send(e);
+    }
+  });
   
