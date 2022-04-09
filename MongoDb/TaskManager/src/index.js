59f8d3dc7321a8655f1053804ba840fed66d2924
Priyansh <priyanshg615@gmail.com>
2022-04-09 23:29:06 +0530

    Mongoose

--- MongoDb/TaskManager/src/index.js
+++ MongoDb/TaskManager/src/index.js
@@ -5,17 +5,14 @@ const Task = require("../src/models/tasks");
 const e = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const UserRouter=require("../src/routers/User")
-const TaskRouter=require("../src/routers/Task")
+const UserRouter = require("../src/routers/User");
+const TaskRouter = require("../src/routers/Task");
 
 app.use(express.json());
 
-app.use(UserRouter)
+app.use(UserRouter);
 
-app.use(TaskRouter)
-
-
-
+app.use(TaskRouter);
 
 app.listen(port, () => {
   console.log("Hurray!!...Server is Running");
