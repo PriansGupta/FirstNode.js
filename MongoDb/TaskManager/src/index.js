0e653fdf5c0429e195f78391fed38f71e1349bf6
Priyansh <priyanshg615@gmail.com>
2022-04-09 23:28:30 +0530

    Mongoose

--- MongoDb/TaskManager/src/index.js
+++ MongoDb/TaskManager/src/index.js
@@ -6,39 +6,13 @@ const e = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const UserRouter=require("../src/routers/User")
+const TaskRouter=require("../src/routers/Task")
 
 app.use(express.json());
 
 app.use(UserRouter)
 
-app.post("/tasks", async (req, res) => {
-  const task = new Task(req.body);
-
-  try {
-    await task.save();
-    res.status(201).send(task);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-
-
-app.get("/tasks/:id", async (req, res) => {
-  const _id = req.params.id;
-
-  try {
-    const task = await Task.findById(_id);
-
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.status(200).send("Task Found: " + task);
-  } catch (e) {
-    res.status(404).send(e);
-  }
-});
-
+app.use(TaskRouter)
 
 
 
--- MongoDb/TaskManager/src/routers/Task.js
+++ MongoDb/TaskManager/src/routers/Task.js
@@ -1,34 +1,29 @@
+const express = require("express");
+const Task = require("../models/tasks");
+const router = new express.Router();
 
-const express=require("express")
-const User=require("../models/user")
-const router =new express.Router();
+router.post("/tasks", async (req, res) => {
+  const task = new Task(req.body);
 
+  try {
+    await task.save();
+    res.status(201).send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
-app.post("/tasks", async (req, res) => {
-    const task = new Task(req.body);
-  
-    try {
-      await task.save();
-      res.status(201).send(task);
-    } catch (e) {
-      res.status(400).send(e);
+router.get("/tasks", async (req, res) => {
+  try {
+    const task = await Task.find({});
+
+    if (!task) {
+      return res.status(404).send();
     }
-  });
-  
-  
-  
-  app.get("/tasks/:id", async (req, res) => {
-    const _id = req.params.id;
-  
-    try {
-      const task = await Task.findById(_id);
-  
-      if (!task) {
-        return res.status(404).send();
-      }
-      res.status(200).send("Task Found: " + task);
-    } catch (e) {
-      res.status(404).send(e);
-    }
-  });
-  
+    res.status(200).send(task);
+  } catch (e) {
+    res.status(404).send(e);
+  }
+});
+
+module.exports = router;
