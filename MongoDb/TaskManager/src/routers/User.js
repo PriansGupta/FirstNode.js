1286164f34a58fd3c677d164df023b6aaa9ff955
Priyansh <priyanshg615@gmail.com>
2022-04-10 23:26:58 +0530

    Mongoose

--- MongoDb/TaskManager/src/routers/User.js
+++ MongoDb/TaskManager/src/routers/User.js
@@ -80,13 +80,12 @@ router.delete("/users/:id", async (req, res) => {
 
 router.post("/users/login", async (req, res) => {
   try {
-    const user = await User.findByCredentials(
-      req.body.email,
-      req.body.password
-    );
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    const token=await user.generateToken();
 
-  res.send(user)
-  } catch (e) {
+  res.send({user,token});
+  }
+   catch (e) {
     res.status(400).send(e);
   }
 });
