8799f7bce25d79e22d330b8e7ce920788cb434a7
Priyansh <priyanshg615@gmail.com>
2022-04-12 19:31:55 +0530

    Mongoose

--- MongoDb/TaskManager/src/middlewares/auth.js
+++ MongoDb/TaskManager/src/middlewares/auth.js
@@ -6,7 +6,7 @@ const auth = async (req, res, next) => {
     const token = req.header("Authorization").replace("Bearer ", "");
     const decoded = jwt.verify(token, "INeedToLearnThis");
 
-    const user = await User.findOne({ _id: decoded._id });
+    const user = await User.findOne({ _id: decoded._id ,"tokens.token":token});
 
     console.log(user);
   } catch (e) {
