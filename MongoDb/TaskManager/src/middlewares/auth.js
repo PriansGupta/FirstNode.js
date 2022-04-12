9a9156dd4b5836f0acd0e7c2f445aecee037f4ab
Priyansh <priyanshg615@gmail.com>
2022-04-12 18:58:47 +0530

    Mongoose

--- MongoDb/TaskManager/src/middlewares/auth.js
+++ MongoDb/TaskManager/src/middlewares/auth.js
@@ -4,3 +4,5 @@ const auth=async (req,res,next)=>{
     console.log("This is a middleware")
 next();
 }
+
+module.exports=auth;
--- MongoDb/TaskManager/src/routers/User.js
+++ MongoDb/TaskManager/src/routers/User.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const User = require("../models/user");
 const router = new express.Router();
+const auth=require("../middlewares/auth")
+
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
@@ -28,7 +30,7 @@ router.get("/users/:id", async (req, res) => {
   }
 });
 
-router.get("/users", async (req, res) => {
+router.get("/users",auth, async (req, res) => {
   try {
     const users = await User.find({});
 
