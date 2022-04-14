6a524396db38c3c9b09495c34a86c7b4c4e6f9a9
Priyansh <priyanshg615@gmail.com>
2022-04-14 13:02:33 +0530

    Token Authentication

--- MongoDb/TaskManager/src/middlewares/auth.js
+++ MongoDb/TaskManager/src/middlewares/auth.js
@@ -16,7 +16,7 @@ const auth = async (req, res, next) => {
     }
 
     req.user = user;
-    // conso
+    // cons
     next();
   } catch (e) {
     res.status(401).send({ error: "Please Authenticate" });
