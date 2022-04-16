7453ca818f81a10b3fd16b0bae1b0a32b9168e3f
Priyansh <priyanshg615@gmail.com>
2022-04-16 13:09:46 +0530

    Token Authentication

--- MongoDb/TaskManager/src/routers/Task.js
+++ MongoDb/TaskManager/src/routers/Task.js
@@ -6,7 +6,8 @@ const router = new express.Router();
 
 router.post("/tasks",auth, async (req, res) => {
   const task = new Task({
-      ...req.body,owner:req.user._id,
+      ...req.body,
+      Owner:req.user._id,
   });
 
   try {
