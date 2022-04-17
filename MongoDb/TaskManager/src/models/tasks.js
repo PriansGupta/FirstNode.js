1924f754c7948a9cc60b55c63107eca41067ec36
Priyansh <priyanshg615@gmail.com>
2022-04-17 22:51:12 +0530

    Token Authentication

--- MongoDb/TaskManager/src/models/tasks.js
+++ MongoDb/TaskManager/src/models/tasks.js
@@ -1,22 +1,25 @@
+const { Schema } = require("mongoose");
 const mongoose = require("mongoose");
 
-const Task = mongoose.model("Tasks", {
-  Description: {
-    type: String,
-    required: true,
-    validate(value) {
-      if (value.length == 0)
-        throw new Error("Description length cannot be zero");
+const TaskSchema=new Schema( {
+    Description: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (value.length == 0)
+          throw new Error("Description length cannot be zero");
+      },
     },
-  },
-  Completed: {
-    type: Boolean,
-    required: true,
-  },
-  Owner: {
-    type: mongoose.Schema.Types.ObjectId,
-    required: true,
-  },
-});
+    Completed: {
+      type: Boolean,
+      required: true,
+    },
+    Owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+  })
+
+const Task = mongoose.model("Tasks",);
 
 module.exports = Task;
