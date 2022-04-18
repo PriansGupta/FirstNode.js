8623706e8714a352ec8efce4411d943a513e598c
Priyansh <priyanshg615@gmail.com>
2022-04-18 12:51:57 +0530

    TaskUser relationship

--- MongoDb/TaskManager/src/models/user.js
+++ MongoDb/TaskManager/src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const Task = require("./tasks");
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -39,6 +40,10 @@ const UserSchema = new mongoose.Schema({
 });
 
 UserSchema.pre("remove",async function(next){
+
+    const user=this;
+    await Task.deleteMany({owner:user._id})
+    next();
 
 })
 
