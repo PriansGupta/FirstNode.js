8aff9a8e6aece4ef00edc33a13bcefe20e2e52c7
Priyansh <priyanshg615@gmail.com>
2022-04-20 16:47:57 +0530

    TaskUser relationship

--- MongoDb/TaskManager/playground/promiseChaining.js
+++ MongoDb/TaskManager/playground/promiseChaining.js
@@ -17,7 +17,7 @@ User.findByIdAndUpdate("62449d528a95f03e701d7c02", { age: 47 })
 Task.findByIdAndDelete("62499dc7b471a4178c993b71")
   .then((user) => {
     console.log(user);
-    return User.countDocuments({ Completed: false });
+    
   })
   .then((result) => {
 
