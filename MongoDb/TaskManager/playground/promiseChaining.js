e5d8b170c5912b8c6b0dfd7b97c9a2cbeb40c853
Priyansh <priyanshg615@gmail.com>
2022-04-20 16:52:32 +0530

    TaskUser relationship

--- MongoDb/TaskManager/playground/promiseChaining.js
+++ MongoDb/TaskManager/playground/promiseChaining.js
@@ -3,5 +3,4 @@ const User = require("../src/models/user");
 const Task = require("../src/models/tasks");
 
 User.findByIdAndUpdate("62449d528a95f03e701d7c02", { age: 47 })
-  .then((user) => {    
-  })
+
