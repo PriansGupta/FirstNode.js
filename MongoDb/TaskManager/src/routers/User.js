9ea79cfddc781c7200e3ecf1ba402a7500d4cf12
Priyansh <priyanshg615@gmail.com>
2022-04-23 23:15:09 +0530

    File Uploads

--- MongoDb/TaskManager/src/routers/User.js
+++ MongoDb/TaskManager/src/routers/User.js
@@ -108,7 +108,7 @@ router.post("/users/logout", auth, async (req, res) => {
   }
 });
 
-router.post("/users/upload/avatar", upload.single("avatar"), (req, res) => {
+router.post("/users/upload/avatar",auth, upload.single("avatar"), (req, res) => {
   res.send();
 },(error,req,res,next)=>{
   res.status(400).send({error:error.message})
