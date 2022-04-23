29be97e73d9b9ba3e1b6f16cf26637cd7ab2050c
Priyansh <priyanshg615@gmail.com>
2022-04-23 19:55:54 +0530

    File Uploads

--- MongoDb/TaskManager/src/index.js
+++ MongoDb/TaskManager/src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const UserRouter = require("../src/routers/User");
 const TaskRouter = require("../src/routers/Task");
-const multer=require("multer")
+const multer = require("multer");
 
 require("../src/db/mongoose");
 
@@ -10,15 +10,17 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-
 app.use(UserRouter);
 app.use(TaskRouter);
 
-const upload=multer({
-  dest:"images"
-})
+const upload = multer({
+  dest: "images",
+});
 
+app.post("/upload", upload.single("upload"), (req, res) => {
+  res.send();
+});
 
 app.listen(port, () => {
-  console.log("Server is Up on "+port);
+  console.log("Server is Up on " + port);
 });
